71d58faf1dcc89494c5c70331d021cd7b6088c09
itsmaheshD <maheshdahe100@gmail.com>
2025-06-28 22:12:57 +0530

    Updated controllers

---
+++ src/controllers/login.controller(authcontroller).js
@@ -0,0 +1,23 @@
+import User from '../models/user.js';
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send('Invalid credentials: user not found');
+    }
+    if (user.password !== password) {
+      return res.status(400).send('Invalid credentials: incorrect password');
+    }
+    res.send(`
+      Login successful.<br>
+      User ID: ${user._id}<br>
+      Name: ${user.name}<br>
+      Role: ${user.role}
+    `);
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).send('Server error');
+  }
+};
