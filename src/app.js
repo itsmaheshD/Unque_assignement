4772cf24a9379bd6306557a2138ae93a66635d41
itsmaheshD <maheshdahe100@gmail.com>
2025-06-29 20:40:21 +0530

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import { connectDB } from './utils/db.utils.js';
-import loginRoutes from './routes/user.routes.js';
-import registerRoutes from './routes/register.route.js';
+import authroutes from './routes/user.routes.js';
+import available_slot from './routes/slotRegister.route.js';
 //import appointmentRoutes from './routes/appointmentRoutes.js';
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/login', loginRoutes);
-app.use('/api/register', registerRoutes);
-
+app.use('/api/auth',authroutes);
+app.use('/api/availability',available_slot);
 connectDB().then(() => {
   app.listen(3000, () => console.log('Server running on port 3000'));
 });
