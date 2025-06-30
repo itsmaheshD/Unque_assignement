34a8a535330e269c592cf236815a78b58ba28d5b
itsmaheshD <maheshdahe100@gmail.com>
2025-06-30 15:12:51 +0530

    Update

--- src/controllers/availability.controller.js
+++ src/controllers/availability.controller.js
@@ -1,16 +1,24 @@
 import Slot_register from '../models/register_slot.models.js';
-
+//controller to book professor appointment with given slot
 export const addSlot = async (req, res) => {
   const { professorId, date, startTime, endTime } = req.body;
-  const slot = new Slot_register({ professorId, date, startTime, endTime });
+  const slot = new Slot_register({ professor: professorId, date, startTime, endTime });
   await slot.save();
   res.json({ message: 'Slot added', slot });
 };
-export const retriveSlots=async(req,res)=>{
-  const {professorId}=req.params;
-  const slot=await Slot_register.find({professorId,isbook:false});
-  if(slot.length===0){
-    return res.json({message:"No slots"});
+
+//function to Check the slots available for a prfessor
+//which in turn will return the slots which are not booked
+export const retriveSlots = async (req, res) => {
+  const { professorId } = req.params;
+  const slot = await Slot_register.find({ professor: professorId, isbook: false });
+  if (slot.length === 0) {
+    return res.json({ message: "No slots" });
   }
-  res.json({slot});
-}
+  res.json({ slot });
+};
+
+
+
+
+
