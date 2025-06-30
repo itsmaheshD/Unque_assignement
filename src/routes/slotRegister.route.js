f5372c47bb0144d4a6d08cf2360110e11d6c73aa
itsmaheshD <maheshdahe100@gmail.com>
2025-06-30 15:11:56 +0530

    Update

--- src/routes/slotRegister.route.js
+++ src/routes/slotRegister.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import {addSlot,retriveSlots} from '../controllers/availability.controller';
+import {addSlot,retriveSlots} from "../controllers/availability.controller.js";
 
 const router=express.Router();
 //Route for checking availability of slots
 router.post('/',addSlot);
+
+//Route for retrieving slots available for a professor
+//Uses the retriveSlots function from controller/availabilty.controller.js file
+//will return unbooked slots
 router.get('/:professorId',retriveSlots);
 export default router;
