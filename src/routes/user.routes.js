59f5881b47062798c646808cdfe7f74b52573212
itsmaheshD <maheshdahe100@gmail.com>
2025-07-06 15:53:40 +0530

    Updated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import { login } from '../controllers/login.controller(authcontroller).js';
 import { register } from '../controllers/user.controllers.js';
 const router = express.Router();
 
+//Endpoints for user authentication
 router.post('/login', login);
 router.post('/register', register);
 
