ba2e115d0ef134729aecc9b7a22713df6c1eb423
itsmaheshD <maheshdahe100@gmail.com>
2025-07-06 15:46:27 +0530

    Updated appointment document

--- src/models/appointment.models.js
+++ src/models/appointment.models.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
+//schema for appointment slots
+//defines the structure of booked slot in database
 const appointmentSchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User_type', required: true },
-  professorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User_type', required: true },
+  studentname: { type: String, ref: 'User_type', required: true },
+  professorname: { type: String, ref: 'User_type', required: true },
   slot: { type: mongoose.Schema.Types.ObjectId, ref: 'Availability', required: true },
   status: { type: String, enum: ['booked', 'cancelled'], default: 'booked' }
 });
