443d59f2f4d02cc4de9af2db3a22ca60ad4d207a
itsmaheshD <143939506+itsmaheshD@users.noreply.github.com>
2025-07-08 21:36:11 +0530

    return logic update

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -8,8 +8,8 @@ export const register = async (req, res) => {
   }
   try {
     const user = await User.create({ name, email, password, role });
-    res.status(201).json({ id: user._id, name: user.name, role: user.role });
+    return  res.status(201).json({ id: user._id, name: user.name, role: user.role });
   } catch {
-    res.status(400).json({ message: 'Useready exists' });
+   return  res.status(400).json({ message: 'Useready exists' });
   }
 };
